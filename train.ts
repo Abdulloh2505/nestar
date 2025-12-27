5f79a11ddeb6d48780b757fc2b317dc7ddd2ae86
Abdulloh2505 <daikarnon.5002@gmail.com>
2025-12-27 09:38:33 +0900

    feat: TRAIN TS and TASK-ZK

---
+++ train.ts
@@ -0,0 +1,15 @@
+ function printNumbers(): void {
+  let count = 1;
+
+  const interval = setInterval(() => {
+    console.log(count);
+    count++;
+
+    if (count > 5) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
+
+printNumbers();
+//natija ishlayaptigit 
