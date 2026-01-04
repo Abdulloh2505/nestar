80d032f47d873676d3012324d1ddb6e21b970999
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-05 06:10:27 +0900

    fix: modify view module to record views

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Query , Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
@@ -11,82 +11,85 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { shapeIntoMongoObjectId } from '../../libs/config';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class MemberResolver {
-    constructor(private readonly memberService: MemberService) {}
+    constructor(private readonly memberService: MemberService) { }
 
     @Mutation(() => Member)
-    public async signup(@Args("input") input: MemberInput ): Promise<Member> {
+    public async signup(@Args("input") input: MemberInput): Promise<Member> {
         console.log('Mutation: signup');
         return this.memberService.signup(input);
-      
-        
+
+
     }
 
 
 
-      @Mutation(() => Member)
-    public async login(@Args("input") input: LoginInput ): Promise<Member> {
-         console.log("Mutation: login");
+    @Mutation(() => Member)
+    public async login(@Args("input") input: LoginInput): Promise<Member> {
+        console.log("Mutation: login");
         return this.memberService.login(input);
-        
+
     }
 
-       @UseGuards(AuthGuard)
+    @UseGuards(AuthGuard)
     @Query(() => String)
     public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
-     console.log('Query: checkAuth');
-    console.log('memberNick:', memberNick);
-       return `Hi ${memberNick}`;
-       }
+        console.log('Query: checkAuth');
+        console.log('memberNick:', memberNick);
+        return `Hi ${memberNick}`;
+    }
 
-      @Roles(MemberType.USER, MemberType.AGENT)
-      @UseGuards(RolesGuard)
-       @Query(() => String)
-       public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
-      console.log('Query: checkAuthRoles');
-      return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId:   ${authMember._id})`;
-}
+    @Roles(MemberType.USER, MemberType.AGENT)
+    @UseGuards(RolesGuard)
+    @Query(() => String)
+    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+        console.log('Query: checkAuthRoles');
+        return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId:   ${authMember._id})`;
+    }
 
 
-       //Authentication 
-       @UseGuards(AuthGuard)
-      @Mutation(() => Member)
-    public async updateMember( 
-    @Args('input') input: MemberUpdate,
-    @AuthMember('_id') memberId: ObjectId
-     ): Promise<Member> {
+    //Authentication 
+    @UseGuards(AuthGuard)
+    @Mutation(() => Member)
+    public async updateMember(
+        @Args('input') input: MemberUpdate,
+        @AuthMember('_id') memberId: ObjectId
+    ): Promise<Member> {
         console.log("Mutation: updateMember");
-     delete input._id;
+        delete input._id;
         return this.memberService.updateMember(memberId, input);
     }
-   
 
+
+
+    @UseGuards(WithoutGuard)
     @Query(() => Member)
-    public async getMember(@Args("memberId") input: string): Promise<Member> {
+    public async getMember(@Args("memberId") input: string, @AuthMember('_id') memberId: ObjectId): Promise<Member> {
         console.log("Query: getMember");
         const targetId = shapeIntoMongoObjectId(input);
-        return this.memberService.getMember(targetId);
+        return this.memberService.getMember(memberId,targetId);
     }
 
 
     /** ADMIN **/
-    
+
     //AUTHERIZATION
     @Roles(MemberType.ADMIN)
     @UseGuards(RolesGuard)
     @Mutation(() => String)
     public async getAllMembersByAdmin(): Promise<string> {
-         return this.memberService. getAllMembersByAdmin() ;
+        return this.memberService.getAllMembersByAdmin();
     }
 
 
-       //Authentication :ADMIN 
-      @Mutation(() => String)
+    //Authentication :ADMIN 
+    @Mutation(() => String)
     public async updateMemberByAdmin(): Promise<string> {
         console.log("Mutation: updateMemberByAdmin");
-         return this.memberService. updateMemberByAdmin() ;
+        return this.memberService.updateMemberByAdmin();
     }
 
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -8,89 +8,113 @@ import { Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/common';
+import { ViewService } from '../view/view.service';
+import { ViewInput } from '../../libs/dto/view/view.input';
+import { ViewGroup } from '../../libs/enums/view.enum';
 
 
 @Injectable()
 export class MemberService {
 
     constructor(@InjectModel('Member') private readonly memberModel: Model<Member>,
-     private authService: AuthService,
-    ) {}
+        private authService: AuthService,
+           private viewService: ViewService,
+    ) { }
 
-    public async signup(input: MemberInput): Promise<Member>{
+    public async signup(input: MemberInput): Promise<Member> {
         //TODO Hash password
-     input.memberPassword = await this.authService.hashPassword(input.memberPassword);
-      
-    try{
-     const result = await this.memberModel.create(input);
-     // TODO Authentication via TOKEN
-      result.accessToken = await this.authService.createToken(result);
-         return result;
-    } catch(err) {
-        console.log('Error, Service.model:', err.message);
-        throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+        input.memberPassword = await this.authService.hashPassword(input.memberPassword);
+
+        try {
+            const result = await this.memberModel.create(input);
+            // TODO Authentication via TOKEN
+            result.accessToken = await this.authService.createToken(result);
+            return result;
+        } catch (err) {
+            console.log('Error, Service.model:', err.message);
+            throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+
+        }
+
 
     }
 
-        
-    }
-    
-    public async login(input: LoginInput): Promise<Member>{
-        const {memberNick, memberPassword } = input;
-        const response:  Member = await this.memberModel
-        .findOne({memberNick: memberNick})
-        .select('+memberPassword')
-        .exec();
+    public async login(input: LoginInput): Promise<Member> {
+        const { memberNick, memberPassword } = input;
+        const response: Member = await this.memberModel
+            .findOne({ memberNick: memberNick })
+            .select('+memberPassword')
+            .exec();
 
-        if(!response || response.memberStatus === MemberStatus.DELETE) {
+        if (!response || response.memberStatus === MemberStatus.DELETE) {
             throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
         } else if (response.memberStatus === MemberStatus.BLOCK) {
-           throw new InternalServerErrorException(Message.BLOCKED_USER);
+            throw new InternalServerErrorException(Message.BLOCKED_USER);
         }
-         
+
         //TODO : COmpear Passwords
-      
+
         const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
-        if(!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+        if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
 
-          response.accessToken = await this.authService.createToken(response);
-         return response;
+        response.accessToken = await this.authService.createToken(response);
+        return response;
     }
 
-    public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member>{
+    public async updateMember(memberId: ObjectId, input: MemberUpdate): Promise<Member> {
         const result: Member = await this.memberModel
-        .findOneAndUpdate({
-            _id: memberId,
-            memberStatus: MemberStatus.ACTIVE,
-         }, 
-         input, 
-        { new: true },
-    ) 
-    .exec();
-     if(!result) throw new InternalServerErrorException(Message.UPLOAD_FAILED);
+            .findOneAndUpdate({
+                _id: memberId,
+                memberStatus: MemberStatus.ACTIVE,
+            },
+                input,
+                { new: true },
+            )
+            .exec();
+        if (!result) throw new InternalServerErrorException(Message.UPLOAD_FAILED);
 
-     result.accessToken = await this.authService.createToken(result);
-         return result;
+        result.accessToken = await this.authService.createToken(result);
+        return result;
     }
 
-    public async getMember(targetId: ObjectId): Promise<Member>{
-        const search: T ={
+    public async getMember(memberId:ObjectId, targetId: ObjectId): Promise<Member> {
+        const search: T = {
             _id: targetId,
             memberStatus: {
                 $in: [MemberStatus.ACTIVE, MemberStatus.BLOCK],
             },
         };
-        const targetMember = await this.memberModel.findOne(search).exec();
-        if(!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
-         return targetMember;
+        const targetMember = await this.memberModel.findOne(search).lean().exec();
+        if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+       if(memberId){
+      
+        //record view
+        const ViewInput = {memberId: memberId, viewRefId: targetId, viewGroup: ViewGroup.MEMBER}
+
+          const newView = await this.viewService.recordView(ViewInput);
+          if(newView) {
+            await this.memberModel.findByIdAndUpdate(search, {$inc: {memberViews: 1}}, {new: true}).exec();
+            targetMember.memberViews++;
+          }
+
+        // increase memberView
+       }
+      
+        return targetMember;
     }
 
-        public async  getAllMembersByAdmin(): Promise<string>{
-         return 'getAllMembersByAdmin executed!';
+
+
+
+
+
+
+    public async getAllMembersByAdmin(): Promise<string> {
+        return 'getAllMembersByAdmin executed!';
     }
 
-    public async updateMemberByAdmin(): Promise<string>{
-         return 'updateMemberByAdmin executed!';
+    public async updateMemberByAdmin(): Promise<string> {
+        return 'updateMemberByAdmin executed!';
     }
 
 
