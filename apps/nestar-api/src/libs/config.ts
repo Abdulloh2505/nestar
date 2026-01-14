6485730d5ffd8158c5e876b2f059dac23ccd65fa
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-14 16:18:48 +0900

    feat:develoop admin reletes Graph QL apis

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -13,6 +13,7 @@ export const availablePropertySorts = [
   'propertyPrice',
 ];
 
+export const availableBoardArticleSorts = ['createdAt', 'updatedAt', 'articleLikes', 'articleViews']
 
 
 /**  IMAGE CONFIGURATION  **/
