c698311f5bdc45208876e46331f7db6d9c49fff9
Abdulloh2505 <daikarnon.5002@gmail.com>
2026-01-21 20:33:48 +0900

    feat: integration of lookuoAuthMemberFollowed complex query busines logig

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberFollowed, lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 
 @Injectable()
 export class FollowService {
@@ -97,7 +97,7 @@ public async getMemberFollowings(
             { $skip: (page - 1) * limit },
             { $limit: limit },
             lookupAuthMemberLiked(memberId, "$followingId"),
-            // meFollowed
+           lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followingId'}),
             lookupFollowingData,
             { $unwind: '$followingData' },
           ],
@@ -137,6 +137,8 @@ public async getMemberFollowers(
             { $limit: limit },
             // meLiked
             lookupAuthMemberLiked(memberId, "$followerId"),
+         lookupAuthMemberFollowed({ followerId: memberId, followingId: '$followerId'}),
+
             lookupFollowerData,
             { $unwind: '$followerData' },
           ],
